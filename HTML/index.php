4c5b03618ec0bdaec2a4edf95d0e57a438bfc816
Jan Schmitte <janschmitte@gmail.com>
2024-09-10 19:48:03 +0200

    modified InsertTestdata added view to CreateDatabase added first data to index.php

--- HTML/index.php
+++ HTML/index.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <?php
+    $rn = "\r\n";
+
     # define path to database
     $db = new SQLite3('/home/pi/TimeTracker/db.sqlite');
 ?>
@@ -14,6 +16,36 @@
         </div>
         <div>
             <h1>Zeiterfassungen der letzten 30 Tage</h1>
+            <table>
+                <tr>
+                    <th>Datum</th>
+                    <th>Wochentag</th>
+                    <th>Blöcke</th>
+                    <th>IST</th>
+                    <th>SOLL</th>
+                    <th>Differenz</th>
+                </tr>
+<?php
+    $results = $db->query('SELECT * FROM v_TrackedHours ORDER BY date DESC');
+    while ($row = $results->fetchArray()) {
+        # print PHP object to browser's console
+        echo '<script>';
+        echo 'var row = ' . json_encode($row) . ';';
+        echo 'console.log(row);';
+        echo '</script>';
+
+
+        echo "\t\t\t\t<tr>" . $rn;
+        echo "\t\t\t\t\t<td>" . $row['date'] . "</td>" . $rn;
+        echo "\t\t\t\t\t<td>" . $row['dayOfWeek'] . "</td>" . $rn;
+        echo "\t\t\t\t\t<td>" . $row['blocks'] . "</td>" . $rn;
+        echo "\t\t\t\t\t<td>" . $row['trackedHoursPerDay'] . "</td>" . $rn;
+        echo "\t\t\t\t\t<td>" . $row['plannedHours'] . "</td>" . $rn;
+        echo "\t\t\t\t\t<td>" . 'DIFF' . "</td>" . $rn;
+        echo "\t\t\t\t</tr>" . $rn;
+    }
+?>
+            </table>
         </div>
     </body>
 </html>
--- SQLite3/CreateDatabase.sql
+++ SQLite3/CreateDatabase.sql
@@ -1,7 +1,7 @@
 CREATE TABLE plannedHours (
     id INT PRIMARY KEY,
     dayOfWeek INT NOT NULL,
-    plannedHours FLOAT NOT NULL
+    plannedHours INT NOT NULL
 );
 
 CREATE TABLE sites (
@@ -23,7 +23,29 @@ CREATE TABLE trackedHours (
 
 CREATE VIEW v_TrackedHours AS
 SELECT
-    strftime('%u', startedAt) [dayOfWeek],
-    (stoppedAt - startedAt) [diff]
-FROM
-    trackedHoursy
+    sub.date,
+    CASE sub.dayOfWeek
+        WHEN '1' THEN 'Mo'
+        WHEN '2' THEN 'Di'
+		WHEN '3' THEN 'Mi'
+		WHEN '4' THEN 'Do'
+		WHEN '5' THEN 'Fr'
+		WHEN '6' THEN 'Sa'
+		WHEN '0' THEN 'So'
+    END [dayOfWeek],
+	COUNT(*) [blocks],
+	STRFTIME('%H:%M:%S', SUM(sub.diffSeconds), 'unixepoch') [trackedHoursPerDay],
+	COALESCE(plannedHours.plannedHours, '0:00:00') [plannedHours]
+FROM (
+    SELECT
+		STRFTIME('%Y-%m-%d', startedAt) [date],
+		(STRFTIME('%w', startedAt)) [dayOfWeek],
+		STRFTIME('%s', stoppedAt) - STRFTIME('%s', startedAt) [diffSeconds]
+	FROM
+		trackedHours
+    ) sub
+	LEFT OUTER JOIN plannedHours ON sub.dayOfWeek = plannedHours.dayOfWeek
+GROUP BY
+    sub.date,
+    sub.dayOfWeek,
+    plannedHours.plannedHours
